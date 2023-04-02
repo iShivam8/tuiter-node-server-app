92e51ff039f18aa41e9cbfb2bf673c8c0586b65e
Shivam Shukla <shivam.p.shukla@medtronic.com>
2023-04-02 14:38:30 -0400

    Tuits Controller

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -1,23 +1,50 @@
 export default [
     {
-        "_id": "123", "topic": "Space", "username": "SpaceX",
-        "handle": "@spacex", "time": "2h", "image": "spacex.png",
-        "title": "SpaceX's Mission",
-        "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
-        "liked": true, "likes": 2345, "replies": 123, "retuits": 432
+        "_id": 123,
+        "topic": "EDM",
+        "userName": "Ultra",
+        "handle": "@ultra",
+        "time": "2h",
+        "image": "../../images/ultra_icon.jpeg",
+        "title": "Ultra Miami 2023 all set with Top tier DJs - Garrix, Zedd, etc.",
+        "tuit": "Ultra returns with Ultra Miami 2023! Get your tickets now!!! GA Premium Sold out. VIP Premium 90% sold out.",
+        "liked": true,
+        "likes": 7942,
+        "replies": 582,
+        "retuits": 720,
+        "disliked": false,
+        "dislikes": 172
     },
     {
-        "_id": "234", "topic": "Traffic", "username": "The Boring Company",
-        "handle": "@boringcompany", "time": "2h", "image": "boring2.jpg",
-        "title": "The Boring Company fixing traffic",
-        "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
-        "liked": true, "likes": 2345, "replies": 321, "retuits": 432
+        "_id": 234,
+        "topic": "News",
+        "userName": "New York Times",
+        "handle": "@nyt",
+        "time": "2h",
+        "image": "../../images/nyt_icon.png",
+        "title": "People tend to listen EDM over indie Pop, 2023 surveys says.",
+        "tuit": "EDM >>>> Indie POP : 2023 Survey Says. What are your thoughts on this?",
+        "liked": false,
+        "likes": 561,
+        "replies": 1042,
+        "retuits": 389,
+        "disliked": true,
+        "dislikes": 629
     },
     {
-        "_id": "345", "topic": "Brain Injuries", "username": "Neurolink",
-        "handle": "@neurolink", "time": "3h", "image": "neurolink.jpg",
-        "title": "Neurolink fixes paralisis and blindness",
-        "tuit": "Neurolink is an American company founded by Elon Musk that specializes in brain to computer integration. The first application will be to cure spinal cord injuries so that paraplegics can walk again. Other applications include curing blindness and mental disorders.",
-        "liked": true, "likes": 3456, "replies": 345, "retuits": 654
-    },
+        "_id": 345,
+        "topic": "POP",
+        "userName": "The ChainSmokers",
+        "handle": "@tcs",
+        "time": "4d",
+        "image": "../../images/tcs_profile.jpeg",
+        "title": "So far so good album by @tcs is topping the POP charts on Spotify.",
+        "tuit": "And we're back with So Far So Good - Solo Mission being my fav!! Go give a listen",
+        "liked": true,
+        "likes": 947,
+        "replies": 640,
+        "retuits": 872,
+        "disliked": false,
+        "dislikes": 103
+    }
 ];
