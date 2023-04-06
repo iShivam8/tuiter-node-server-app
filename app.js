f4fdd580cad6f8c76faff2a8461ea5afdfd28d2e
Shivam Shukla <shivam.p.shukla@medtronic.com>
2023-04-06 16:51:52 -0400

    Tested DB CONNECTION STRING

--- app.js
+++ app.js
@@ -6,10 +6,17 @@ import HelloController from "./controllers/hello-controller.js"
 import UsersController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 
+const DB_CONNECTION_STRING
+    = `mongodb+srv://iShivam:MongoDB8@cluster-tuiter.jdgh9yh.mongodb.net/tuiter?retryWrites=true&w=majority`;
+
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
     || 'mongodb://127.0.0.1:27017/tuiter'
-mongoose.connect(CONNECTION_STRING);
-//mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
+
+//console.log("DB Connection String Value: " + CONNECTION_STRING);
+mongoose.connect(CONNECTION_STRING)
+    .then(function() {console.log("Successfully connected to Mongo DB Atlas!")})
+    .catch(function() {console.log("Failed to connect to Mongo DB!")});
+//mongoose.connect('mongodb://127.0.0.1:27017/tuiter')
 
 const app = express()
 app.use(cors())
